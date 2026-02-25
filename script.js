b7382baed92ef3e306787ab463763bead8b301a2
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-02-25 21:46:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const signupForm = document.getElementById('signupForm');
+
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // Stop page refresh
+  
+  // Collect data
+  const email = document.getElementById('emailInput').value;
+  const password = document.getElementById('passwordInput').value;
+  const confirmpassword = document.getElementById('confirmPassword').value;
+
+  console.log("Signing up with:", email, password);
+
+  // Close the modal programmatically after success
+  const modalElement = document.getElementById('signupModal');
+  const modalInstance = bootstrap.Modal.getInstance(modalElement);
+  modalInstance.hide();
+  
+  alert("Account created successfully!");
+});
